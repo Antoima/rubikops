4322a9e9b8518797f560e210c14deac0f0b33e81
Alexander <alexander.antoima@gmail.com>
2024-12-04 19:23:28 -0400

    style: improve text and button visibility

---
+++ src/js/navbar.js
@@ -0,0 +1,9 @@
+// JavaScript para cambiar el color del navbar al hacer scroll
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
