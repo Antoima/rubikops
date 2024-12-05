1226eaa08a22d55f30104e553f6766838613ef76
Alexander <alexander.antoima@gmail.com>
2024-12-04 22:15:06 -0400

    feat:add resposive portfolio

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,9 +1,20 @@
-// JavaScript para cambiar el color del navbar al hacer scroll
-window.addEventListener("scroll", function () {
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.getElementById("menu-toggle");
+  const menu = document.getElementById("menu");
   const navbar = document.querySelector(".navbar");
-  if (window.scrollY > 50) {
-    navbar.classList.add("scrolled");
-  } else {
-    navbar.classList.remove("scrolled");
-  }
+
+  // Toggle menu visibility on mobile
+  menuToggle.addEventListener("click", function () {
+    console.log("Menu toggle clicked"); // Agrega este log
+    menu.classList.toggle("hidden");
+  });
+
+  // Change navbar style on scroll
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 50) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  });
 });
