b42b225eee138bf80e1f88d19420b1e813c026c6
Alexander <alexander.antoima@gmail.com>
2024-12-04 23:15:45 -0400

    fix swiper slider

--- src/js/portafolio.js
+++ src/js/portafolio.js
@@ -1,28 +1,31 @@
-// Inicializa Swiper
-var swiper = new Swiper(".swiper-container", {
-  slidesPerView: 1,
-  spaceBetween: 10,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    640: {
-      slidesPerView: 1,
-      spaceBetween: 20,
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper(".swiper-container", {
+    slidesPerView: 1, // Muestra una diapositiva a la vez
+    spaceBetween: 10, // Espacio entre diapositivas
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 40,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
     },
-    1024: {
-      slidesPerView: 3,
-      spaceBetween: 50,
+    breakpoints: {
+      // Cuando la ventana sea >= 640px
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      // Cuando la ventana sea >= 768px
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      // Cuando la ventana sea >= 1024px
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
     },
-  },
+  });
 });
