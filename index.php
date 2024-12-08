41df4499fed00720ab28f2f0639ebcb361a95063
Alexander <alexander.antoima@gmail.com>
2024-12-08 18:24:02 -0400

    refactor: improve meta tags for better seo and usability

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Rubikops - Your partner in digital development. We offer branding, web design, platform management, and more.">
     <title>Rubikops</title>
     <!-- Incluye Tailwind CSS desde un CDN -->
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
@@ -15,6 +16,13 @@
     <!-- Incluye el archivo CSS externo config -->
     <link href="src/css/custom.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Roboto', sans-serif;
+        }
+    </style>
 
 </head>
 <body class="bg-gray-10">
